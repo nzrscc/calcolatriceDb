f3a2395dae20e38c1ff80b9b6b6c8c7ebfc7b5d7
nzrscc <nzrscc@gmail.com>
2019-01-28 15:17:18 +0100

    Controllore su op

--- src/com/company/calcolatrice.java
+++ src/com/company/calcolatrice.java
@@ -1,11 +1,13 @@
 package com.company;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class calcolatrice {
     private double op1;
     private double op2;
     private double risultato;
+    private String controlloOp="false";
     private Scanner in=new Scanner(System.in);
 
     public void setRisultato(double risultato)
@@ -20,10 +22,46 @@ public class calcolatrice {
 
     public void setOp()
     {
-        System.out.print("Inserire il primo operatore:\t");
-        this.op1=this.in.nextDouble();
-        System.out.print("Inserire il secondo operatore:\t");
-        this.op2=this.in.nextDouble();
+        this.setOp1();
+        this.setOp2();
+    }
+
+    public void setOp1()
+    {
+        controlloOp="false";
+        while(this.controlloOp.equals("false"))
+        {
+            System.out.print("Inserire il primo operatore:\t");
+            try
+            {
+                this.op1=this.in.nextDouble();
+                this.controlloOp="true";
+            }catch(InputMismatchException ime)
+            {
+                this.controlloOp="false";
+                System.out.println("Valore non valido!!");
+                in.nextLine();
+            }
+        }
+    }
+
+    public void setOp2()
+    {
+        controlloOp="false";
+        while(this.controlloOp.equals("false"))
+        {
+            System.out.print("Inserire il secondo operatore:\t");
+            try
+            {
+                this.op2=this.in.nextDouble();
+                this.controlloOp="true";
+            }catch(InputMismatchException ime)
+            {
+                this.controlloOp="false";
+                System.out.println("Valore non valido!!");
+                in.nextLine();
+            }
+        }
     }
 
     public double somma()
